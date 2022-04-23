31bfd96d66db56b2b5d55dd621d3c1a4a3aa3ae2
lyq-821 <1139098992@qq.com>
2022-04-23 16:12:55 +0800

    1

--- pages/agent/index.vue
+++ pages/agent/index.vue
@@ -1,11 +1,20 @@
 <template>
 	<view>
-		
+		<uniNavbar title="代理赚钱"></uniNavbar>
+		<uniFooter></uniFooter>
 	</view>
 </template>
 
 <script>
+	//引入组件
+	import uniNavbar from '@/components/uni-navbar.vue'
+	import uniFooter from '@/components/uni-footer.vue'
 	export default {
+		//使用组件
+		components:{
+			uniNavbar,
+			uniFooter
+		},
 		data() {
 			return {
 				
